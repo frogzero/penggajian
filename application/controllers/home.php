4919bf7cab4aa903e9a60e8141433c141a3b8c41
Unknown <eko.rme@gmail.com>
2017-11-05 12:19:41 +0700

    login
    login and home

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -3,6 +3,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
+
+	public function index()
+	{
+		$this->load->view('login');
+		
+	}
+
 	public function admin()
 	{
 		$this->load->view('index');
