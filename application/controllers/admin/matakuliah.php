a035e4a655410bf95d7865e529b174cb38792020
frogzero <eko.rme@gmail.com>
2017-11-08 13:31:05 +0700

    coba coba dari cmd

--- application/controllers/admin/matakuliah.php
+++ application/controllers/admin/matakuliah.php
@@ -47,4 +47,4 @@ class Matakuliah extends CI_Controller {
 
 /* End of file matakuliah.php */
 /* Location: ./application/controllers/matakuliah.php */
-// tester coba coba
+// ganti dari cmd coba coba
