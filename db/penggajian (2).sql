819c3cd28377b011ac4983bcee65dcd2affd8978
Unknown <eko.rme@gmail.com>
2017-11-11 10:01:37 +0700

    update db

--- db/penggajian (2).sql
+++ db/penggajian (2).sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 15, 2017 at 04:38 PM
--- Server version: 10.1.10-MariaDB
--- PHP Version: 5.5.33
+-- Generation Time: 11 Nov 2017 pada 03.56
+-- Versi Server: 10.1.9-MariaDB
+-- PHP Version: 5.6.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,13 +17,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `penggajianstaff`
+-- Database: `penggajian`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
+-- Struktur dari tabel `admin`
 --
 
 CREATE TABLE `admin` (
@@ -34,63 +34,90 @@ CREATE TABLE `admin` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `admin`
+-- Dumping data untuk tabel `admin`
 --
 
 INSERT INTO `admin` (`id_admin`, `id_user`, `email_admin`, `password_admin`) VALUES
-(4545, 1, 'adminnie@gmail.com', 'minnieminniemo');
+(4545, 1, 'adminnie@gmail.com', '098f6bcd4621d373cade4e832627b4f6');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `matakuliah`
+-- Struktur dari tabel `kelas`
+--
+
+CREATE TABLE `kelas` (
+  `id_kelas` int(11) NOT NULL,
+  `nama_kelas` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `kelas`
+--
+
+INSERT INTO `kelas` (`id_kelas`, `nama_kelas`) VALUES
+(1, 'A'),
+(2, 'B'),
+(3, 'C'),
+(4, 'D');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `matakuliah`
 --
 
 CREATE TABLE `matakuliah` (
-  `kodeMK` char(6) NOT NULL,
+  `kodeMK` char(7) NOT NULL,
   `namaMK` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `matakuliah`
+-- Dumping data untuk tabel `matakuliah`
 --
 
 INSERT INTO `matakuliah` (`kodeMK`, `namaMK`) VALUES
 ('TI-101', 'Agama Islam 1'),
 ('TI-102', 'Bahasa Inggris 1'),
 ('TI-103', 'Fisika dan Elektronika'),
-('TI-104', 'Dasar Teknologi Informasi');
+('TI-104', 'Dasar Teknologi Informasi'),
+('TI-105', 'Applikasi Produktifitas Kerja');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `penggajian`
+-- Struktur dari tabel `penggajian`
 --
 
 CREATE TABLE `penggajian` (
   `id_penggajian` int(11) NOT NULL,
-  `id_presensi` int(11) DEFAULT NULL,
   `status` char(10) NOT NULL,
   `nip` char(5) NOT NULL,
-  `kodeMK` char(6) NOT NULL,
+  `kodeMK` char(7) NOT NULL,
+  `id_kelas` int(1) NOT NULL,
   `totalSesi` int(11) NOT NULL,
   `nominal` enum('100000','60000','30000') NOT NULL,
   `totalGaji` float NOT NULL,
+  `bulan` char(2) NOT NULL,
+  `tahun` char(4) NOT NULL,
   `tanggalditerima` date NOT NULL,
-  `statusPengiriman` enum('1','0') NOT NULL
+  `statusPengiriman` enum('0','1') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `penggajian`
+-- Dumping data untuk tabel `penggajian`
 --
 
-INSERT INTO `penggajian` (`id_penggajian`, `id_presensi`, `status`, `nip`, `kodeMK`, `totalSesi`, `nominal`, `totalGaji`, `tanggalditerima`, `statusPengiriman`) VALUES
-(1, NULL, 'Dosen', 'D01', 'TI-101', 4, '100000', 400000, '2017-10-30', '1');
+INSERT INTO `penggajian` (`id_penggajian`, `status`, `nip`, `kodeMK`, `id_kelas`, `totalSesi`, `nominal`, `totalGaji`, `bulan`, `tahun`, `tanggalditerima`, `statusPengiriman`) VALUES
+(5, 'Asisten', 'A01', 'TI-101', 2, 4, '30000', 60000, '10', '2017', '0000-00-00', '0'),
+(6, 'Asisten', 'A01', 'TI-101', 1, 1, '30000', 30000, '10', '2017', '0000-00-00', '0'),
+(7, 'Asisten', 'A01', 'TI-101', 2, 4, '30000', 60000, '10', '2017', '0000-00-00', '1'),
+(8, 'Asisten', 'A01', 'TI-101', 1, 1, '30000', 30000, '10', '2017', '0000-00-00', '1');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `presensi`
+-- Struktur dari tabel `presensi`
 --
 
 CREATE TABLE `presensi` (
@@ -98,35 +125,33 @@ CREATE TABLE `presensi` (
   `tanggal` date NOT NULL,
   `status` char(10) NOT NULL,
   `nip` char(5) NOT NULL,
-  `kodeMK` char(6) NOT NULL,
-  `kelas` char(1) NOT NULL,
+  `kodeMK` char(7) NOT NULL,
+  `id_kelas` int(10) NOT NULL,
   `sesi` int(11) NOT NULL,
-  `kehadiran` enum('1','0') NOT NULL
+  `kehadiran` enum('0','1') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `presensi`
+-- Dumping data untuk tabel `presensi`
 --
 
-INSERT INTO `presensi` (`id_presensi`, `tanggal`, `status`, `nip`, `kodeMK`, `kelas`, `sesi`, `kehadiran`) VALUES
-(1, '2017-10-02', 'Dosen', 'D01', 'TI-101', 'A', 1, '1'),
-(3, '2017-10-09', 'Dosen', 'D01', 'TI-101', 'A', 2, '1'),
-(4, '2017-10-16', 'Dosen', 'D01', 'TI-101', 'A', 3, '1'),
-(5, '2017-10-23', 'Dosen', 'D01', 'TI-101', 'A', 4, '1'),
-(16, '2017-10-03', 'Instruktur', 'I01', 'TI-102', 'B', 1, '1'),
-(17, '2017-10-04', 'Instruktur', 'I01', 'TI-102', 'C', 1, '1'),
-(18, '2017-10-10', 'Instruktur', 'I01', 'TI-102', 'B', 2, '1'),
-(19, '2017-10-11', 'Instruktur', 'I01', 'TI-102', 'C', 2, '0'),
-(20, '2017-10-17', 'Instruktur', 'I01', 'TI-102', 'B', 3, '1'),
-(21, '2017-10-18', 'Instruktur', 'I01', 'TI-102', 'C', 3, '1'),
-(22, '2017-10-24', 'Instruktur', 'I01', 'TI-102', 'B', 4, '1'),
-(23, '2017-10-25', 'Instruktur', 'I01', 'TI-102', 'C', 4, '1'),
-(24, '2017-10-31', 'Instruktur', 'I01', 'TI-102', 'B', 5, '0');
+INSERT INTO `presensi` (`id_presensi`, `tanggal`, `status`, `nip`, `kodeMK`, `id_kelas`, `sesi`, `kehadiran`) VALUES
+(25, '2017-10-06', 'Dosen', 'D01', 'TI-101', 1, 1, '0'),
+(26, '2017-10-06', 'Dosen', 'D01', 'TI-101', 1, 2, '0'),
+(27, '2017-10-13', 'Dosen', 'D01', 'TI-101', 1, 3, '0'),
+(28, '2017-10-13', 'Dosen', 'D01', 'TI-101', 1, 4, '0'),
+(29, '2017-10-06', 'Asisten', 'A01', 'TI-101', 2, 1, '1'),
+(30, '2017-10-06', 'Asisten', 'A01', 'TI-101', 2, 2, '1'),
+(31, '2017-10-13', 'Asisten', 'A01', 'TI-101', 2, 3, '0'),
+(32, '2017-10-13', 'Asisten', 'A01', 'TI-101', 2, 4, '0'),
+(33, '2017-10-23', 'Instruktur', 'I01', 'TI-102', 3, 1, '0'),
+(34, '2017-10-30', 'Instruktur', 'I01', 'TI-102', 3, 2, '0'),
+(38, '2017-11-10', 'Asisten', 'A01', 'TI-101', 2, 5, '');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `staff`
+-- Struktur dari tabel `staff`
 --
 
 CREATE TABLE `staff` (
@@ -141,18 +166,20 @@ CREATE TABLE `staff` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `staff`
+-- Dumping data untuk tabel `staff`
 --
 
 INSERT INTO `staff` (`nip`, `id_user`, `nama_staff`, `status`, `alamat_staff`, `nohp_staff`, `email_staff`, `password_staff`) VALUES
 ('A01', 2, 'Ohno Satoshi', 'Asisten', 'Tokyo', '934567891', 'satoshiohno@yahoo.jp', 'satoshiiohno'),
+('A02', 2, 'Ninomiya Kazunari', 'Asisten', 'Tokyo', '86523456890', 'ninomiyakazunari@yahoo.jp', 'kazunarininomiya'),
 ('D01', 2, 'Nisrina Akbar Rizky Putri, S.T.', 'Dosen', 'Jalan Godean, perumahan nogotirto', '085726541777', 'nisrinaarp@gmail.com', 'nisrinaarp9876'),
+('E01', 2, 'EKo Rismaryanto', 'Instruktur', 'admin', '0293', 'eko.rme@gmail.com', '707c1ee5c3aea98e23f3c0801f0a0c61'),
 ('I01', 2, 'Sakurai Sho', 'Instruktur', 'Tokyo, Japan', '975367112', 'sakuraisho@yahoo.jp', 'shosakuraii');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `status_staff`
+-- Struktur dari tabel `status_staff`
 --
 
 CREATE TABLE `status_staff` (
@@ -161,7 +188,7 @@ CREATE TABLE `status_staff` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `status_staff`
+-- Dumping data untuk tabel `status_staff`
 --
 
 INSERT INTO `status_staff` (`status`, `nominal`) VALUES
@@ -172,7 +199,7 @@ INSERT INTO `status_staff` (`status`, `nominal`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Struktur dari tabel `user`
 --
 
 CREATE TABLE `user` (
@@ -181,64 +208,12 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `user`
+-- Dumping data untuk tabel `user`
 --
 
 INSERT INTO `user` (`id_user`, `role`) VALUES
 (1, 'Admin'),
 (2, 'Staff');
-
--- --------------------------------------------------------
-
---
--- Stand-in structure for view `vpenggajian`
---
-CREATE TABLE `vpenggajian` (
-`id_penggajian` int(11)
-,`id_presensi` int(11)
-,`status` char(10)
-,`nip` char(5)
-,`kodeMK` char(6)
-,`totalSesi` int(11)
-,`nominal` enum('100000','60000','30000')
-,`totalGaji` float
-,`tanggalditerima` date
-,`statusPengiriman` enum('1','0')
-);
-
--- --------------------------------------------------------
-
---
--- Stand-in structure for view `vpresensi`
---
-CREATE TABLE `vpresensi` (
-`id_presensi` int(11)
-,`tanggal` date
-,`status` char(10)
-,`nip` char(5)
-,`kodeMK` char(6)
-,`kelas` char(1)
-,`sesi` int(11)
-,`kehadiran` enum('1','0')
-);
-
--- --------------------------------------------------------
-
---
--- Structure for view `vpenggajian`
---
-DROP TABLE IF EXISTS `vpenggajian`;
-
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vpenggajian`  AS  select `penggajian`.`id_penggajian` AS `id_penggajian`,`penggajian`.`id_presensi` AS `id_presensi`,`penggajian`.`status` AS `status`,`penggajian`.`nip` AS `nip`,`penggajian`.`kodeMK` AS `kodeMK`,`penggajian`.`totalSesi` AS `totalSesi`,`penggajian`.`nominal` AS `nominal`,`penggajian`.`totalGaji` AS `totalGaji`,`penggajian`.`tanggalditerima` AS `tanggalditerima`,`penggajian`.`statusPengiriman` AS `statusPengiriman` from `penggajian` where (`penggajian`.`statusPengiriman` in ('1','0')) WITH CASCADED CHECK OPTION ;
-
--- --------------------------------------------------------
-
---
--- Structure for view `vpresensi`
---
-DROP TABLE IF EXISTS `vpresensi`;
-
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vpresensi`  AS  select `presensi`.`id_presensi` AS `id_presensi`,`presensi`.`tanggal` AS `tanggal`,`presensi`.`status` AS `status`,`presensi`.`nip` AS `nip`,`presensi`.`kodeMK` AS `kodeMK`,`presensi`.`kelas` AS `kelas`,`presensi`.`sesi` AS `sesi`,`presensi`.`kehadiran` AS `kehadiran` from `presensi` where (`presensi`.`kehadiran` in ('1','0')) WITH CASCADED CHECK OPTION ;
 
 --
 -- Indexes for dumped tables
@@ -252,6 +227,12 @@ ALTER TABLE `admin`
   ADD KEY `fk_admin` (`id_user`);
 
 --
+-- Indexes for table `kelas`
+--
+ALTER TABLE `kelas`
+  ADD PRIMARY KEY (`id_kelas`);
+
+--
 -- Indexes for table `matakuliah`
 --
 ALTER TABLE `matakuliah`
@@ -262,10 +243,10 @@ ALTER TABLE `matakuliah`
 --
 ALTER TABLE `penggajian`
   ADD PRIMARY KEY (`id_penggajian`),
-  ADD KEY `id_presensi` (`id_presensi`),
   ADD KEY `status` (`status`),
   ADD KEY `nip` (`nip`),
-  ADD KEY `kodeMK` (`kodeMK`) USING BTREE;
+  ADD KEY `kodeMK` (`kodeMK`) USING BTREE,
+  ADD KEY `penggajian_ibfk_5` (`id_kelas`);
 
 --
 -- Indexes for table `presensi`
@@ -274,7 +255,8 @@ ALTER TABLE `presensi`
   ADD PRIMARY KEY (`id_presensi`),
   ADD KEY `status` (`status`),
   ADD KEY `kodeMK` (`kodeMK`),
-  ADD KEY `nip` (`nip`) USING BTREE;
+  ADD KEY `nip` (`nip`) USING BTREE,
+  ADD KEY `presensi_ibfk_4` (`id_kelas`);
 
 --
 -- Indexes for table `staff`
@@ -306,49 +288,55 @@ ALTER TABLE `user`
 ALTER TABLE `admin`
   MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4546;
 --
+-- AUTO_INCREMENT for table `kelas`
+--
+ALTER TABLE `kelas`
+  MODIFY `id_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
 -- AUTO_INCREMENT for table `penggajian`
 --
 ALTER TABLE `penggajian`
-  MODIFY `id_penggajian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_penggajian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `presensi`
 --
 ALTER TABLE `presensi`
-  MODIFY `id_presensi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id_presensi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
--- Constraints for dumped tables
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Constraints for table `admin`
+-- Ketidakleluasaan untuk tabel `admin`
 --
 ALTER TABLE `admin`
   ADD CONSTRAINT `admin_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
 
 --
--- Constraints for table `penggajian`
+-- Ketidakleluasaan untuk tabel `penggajian`
 --
 ALTER TABLE `penggajian`
-  ADD CONSTRAINT `penggajian_ibfk_1` FOREIGN KEY (`id_presensi`) REFERENCES `presensi` (`id_presensi`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `penggajian_ibfk_2` FOREIGN KEY (`status`) REFERENCES `status_staff` (`status`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `penggajian_ibfk_3` FOREIGN KEY (`nip`) REFERENCES `staff` (`nip`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `penggajian_ibfk_4` FOREIGN KEY (`kodeMK`) REFERENCES `matakuliah` (`kodeMK`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `penggajian_ibfk_4` FOREIGN KEY (`kodeMK`) REFERENCES `matakuliah` (`kodeMK`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `penggajian_ibfk_5` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `presensi`
+-- Ketidakleluasaan untuk tabel `presensi`
 --
 ALTER TABLE `presensi`
   ADD CONSTRAINT `presensi_ibfk_1` FOREIGN KEY (`status`) REFERENCES `status_staff` (`status`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `presensi_ibfk_2` FOREIGN KEY (`nip`) REFERENCES `staff` (`nip`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `presensi_ibfk_3` FOREIGN KEY (`kodeMK`) REFERENCES `matakuliah` (`kodeMK`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `presensi_ibfk_3` FOREIGN KEY (`kodeMK`) REFERENCES `matakuliah` (`kodeMK`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `presensi_ibfk_4` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `staff`
+-- Ketidakleluasaan untuk tabel `staff`
 --
 ALTER TABLE `staff`
   ADD CONSTRAINT `staff_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`),
