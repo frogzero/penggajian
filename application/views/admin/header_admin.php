6420464a8187702052e365b1de2e57591a3d7a71
Eko Pc <eko.rme@gmail.com>
2017-11-12 07:52:03 +0700

    fix echo

--- application/views/admin/header_admin.php
+++ application/views/admin/header_admin.php
@@ -1,26 +1,26 @@
 <div id='cssmenu'>
   <ul>
-      <li><a href='<?=site_url('admin/staff/')?>'><span>Staff</span></a></li>
-      <li><a href='<?=site_url('admin/matakuliah')?>'><span>Mata Kuliah</span></a></li>
-      <li><a href='<?=site_url('admin/status_staff')?>'><span>Status Staff</span></a></li>
-       <li><a href='<?=site_url('admin//kelas')?>'><span>Kelas</span></a></li>
+      <li><a href='<?php echo site_url('admin/staff/')?>'><span>Staff</span></a></li>
+      <li><a href='<?php echo site_url('admin/matakuliah')?>'><span>Mata Kuliah</span></a></li>
+      <li><a href='<?php echo site_url('admin/status_staff')?>'><span>Status Staff</span></a></li>
+       <li><a href='<?php echo site_url('admin//kelas')?>'><span>Kelas</span></a></li>
       <li><a href='#'><span>Presensi</span></a>
          <ul>
-            <li class='has-sub'><a href='<?=site_url('admin/presensi/tambah_presensi')?>'><span>Tambah Presensi</span></a>
+            <li class='has-sub'><a href='<?php echo site_url('admin/presensi/tambah_presensi')?>'><span>Tambah Presensi</span></a>
             </li>
-            <li class='has-sub'><a href='<?=site_url('admin/presensi')?>'><span>Lihat Data Presensi</span></a>
+            <li class='has-sub'><a href='<?php echo site_url('admin/presensi')?>'><span>Lihat Data Presensi</span></a>
             </li>
          </ul>
       </li>
       <li><a href='#'><span>Penggajian</span></a>
          <ul>
-            <li class='has-sub'><a href='<?=site_url('admin/penggajian/tambah_data_penggajian')?>'><span>Hitung Gaji</span></a>
+            <li class='has-sub'><a href='<?php echo site_url('admin/penggajian/tambah_data_penggajian')?>'><span>Hitung Gaji</span></a>
             </li>
-            <li class='has-sub'><a href='<?=site_url('admin/penggajian')?>'><span>Lihat Data Gaji</span></a>
+            <li class='has-sub'><a href='<?php echo site_url('admin/penggajian')?>'><span>Lihat Data Gaji</span></a>
             </li>
          </ul>
    </li>
-   <li style="float: right"><a href='<?=site_url('home/log_out')?>'><span>Logout</span></a></li>
-   <li style="float: right"><a href='<?=site_url('home/akun')?>'><span>Akun</span></a></li>
+   <li style="float: right"><a href='<?php echo site_url('home/log_out')?>'><span>Logout</span></a></li>
+   <li style="float: right"><a href='<?php echo site_url('home/akun')?>'><span>Akun</span></a></li>
 </ul>
 </div>
