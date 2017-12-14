72857e61947dc1e08eaeb0b5fc4f668f748dbba5
Unknown <eko.rme@gmail.com>
2017-12-14 13:58:22 +0700

    laporan penggajian

--- application/views/index.php
+++ application/views/index.php
@@ -15,9 +15,7 @@
 
     <script src="<?php echo base_url()?>assets/js/buttons.print.min.js"></script>
     <script src="<?php echo base_url()?>assets/js/dataTables.buttons.min.js"></script>
-    
- <!--    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script> -->
+   
 
 
     <link rel="stylesheet" href="<?php echo base_url();?>assets/TambahStaff/tanggal.css">
